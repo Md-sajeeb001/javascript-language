6d20685882d2b3185973e695081b5f608f91355d
Mdsajeeb <mohammadsajeeb890@email.com>
2024-08-01 07:00:09 +0600

    learn javascript

---
+++ project.js
@@ -0,0 +1,43 @@
+// toFixd() method ar kaj hocche decimal ba dosomik ar por koto gor dekte chacchi ta neya kaj kore!
+
+// let a = 5.769263;
+
+// console.log(a.toFixed(3));
+
+// and toPrecision() method ar kaj hocche decimel ba total koto gor dekte chacchi ! 
+// console.log(typeof a.toPrecision(2));
+
+// Number to - toString() method;
+
+// let num = 20;
+
+// num = toString(num)
+// console.log(num);
+
+
+// let stringNum = "30";
+
+// stringNum = parseInt(stringNum);
+// console.log(typeof stringNum + " 30");
+
+// use of Number method to change the data type !
+// let n = "0.3";
+
+// n = Number(n);
+// console.log(n);
+// console.log(Number("30"));
+
+// Libary function fot string !
+
+// let number = prompt("Enter Your Name : ")
+// document.write( "Number fo character " + number.length)
+
+// let fPerson = 'Mohammd ';
+// let lPerson = 'Sajeeb';
+//  let fName = fPerson + lPerson;
+
+//  document.write(fName); 
+//  document.write(fName.length);
+//  document.write(fName.toUpperCase())
+// document.write(fPerson + lPerson)
+
---
+++ script.js
@@ -0,0 +1,283 @@
+// const cars = ["A", "B", "C", "D", "E"];
+// for(let i = 0, j = 2; i < cars.length; i++){
+//   console.log(i);
+//   console.log(cars[i]);
+//   console.log(j);
+//   console.log(cars[j]);
+// }
+
+// const person = { fname:"jhon", lname:"deo", age:34};
+
+// for(let x in person){
+//    console.log(x);
+//    console.log(person[x]);
+// }
+
+// const a = "sajeeb";
+
+// for(let x of a){
+//     console.log(x);
+// }
+
+// const num = [ 3, 4, 5, 1, 2, 6,];
+
+// for(let y of num){
+//     console.log(y[num]);
+// }
+
+// const numItems = ["A", "B", "C", "D", "E"];
+
+// for(let numlink = 0; numlink < numItems.length; numlink++){
+//     console.log(numlink);
+//     console.log(numItems[numlink]);
+// }
+
+// const personName = {fname:"Mohammd", lname:"Sajeeb", Age:21, htown:"cumilla"};
+
+// for(let person in personName){
+//     console.log(person);
+//     console.log(personName[person]);
+//     console.log(personName['lname']);
+//     console.log(personName['Age']);
+// }
+
+
+// const personNum = [ 42, 52, 12, 65, 98 ,12,];
+// for(let num of personNum){
+//     console.log(num);
+// }
+
+// let i = 0;
+// let text = "";
+// while (i < 10){
+//     text += "the number is " + i;
+//     i++;
+// }
+// console.log(text);
+
+// let ivAlue = 0;
+// let text = "";
+// while(ivAlue < 5){
+//     text += "this number is" + ivAlue;
+//     ivAlue++;
+// }
+// console.log(text);
+
+// let a = 10;
+// let b = 5;
+
+// if(a < 5){
+//   console.log("it's true");
+// }
+// else if(a < b){
+//   console.log("it's false");
+// }
+// else {
+//   console.log("both are wrong");
+// };
+
+// let fname = "Mohammad";
+// let lname = "Sajeeb";
+
+// switch(lname){
+//    case fname:
+//     console.log("it's my first name");
+//     break;
+
+//     case lname:
+//         console.log("it's my last name");
+//         break;
+// }
+
+// let fNum = 100;
+// let lNum = 200;
+
+// switch(lNum){
+//     case 200:
+//    console.log("the number is 200");
+//    break;
+   
+//    case 54:
+//     console.log("the number is 100");
+//     break;
+    
+//     default: 
+//         console.log("they number is not a correct");
+// }
+
+// let myName = 5;
+// let nam = "";
+
+// while(myName < 10){
+//     nam += "this is" + myName;
+//     myName++;
+// }
+
+// console.log(nam);
+
+
+  // function sleep(ourName, taka) {
+  //   console.log("My Name Is " + ourName +  "and i have" +  taka);
+  // }
+  //  console.log(ourName);
+  // sleep("Sajeeb", "20 taka");
+  // sleep("josim", "30 taka");
+  // sleep("korim", "40 taka");
+  // sleep("sojol", "50 taka");
+  // sleep("shakil", "60 taka");
+  // sleep("babul", "1 taka");
+
+
+  // function myfunction(names, country){
+  //   console.log("Hi I'm" + names + "and I am from," + country);
+  //   return ;
+  // }
+
+  // myfunction(" Sajeeb ", " Bangladesh ");
+  // myfunction(" sojol ", " dubai ");
+  // myfunction(" babul ", " soudia ");
+  // myfunction(" ahesan ", " india ");
+
+  // let sajeebHas = 100;
+  // let sojolHas = 50;
+
+  // if(sajeebHas < sojolHas){
+  //   console.log("Sajeeb is win");
+  // }
+  // else if(sojolHas < 100){
+  //   console.log("Sajeeb Has 100 taka");
+  // }
+  // else{
+  //   console.log("no one win");
+  // }
+
+  // let a = 10;
+  // let b = 5;
+
+  // switch(b, a){
+  //    case 10:
+  //     console.log("this is 10");
+  //      break;
+
+  //    case 5:
+  //     console.log("this is 5");
+  //      break;
+     
+  //    default: console.log("no one correct");
+  // }
+
+  // const familyMember = [1, 2, 3, 4, 5, 6, 7, 8,];
+
+  // for(let member = 0; member < familyMember.length; member++){
+    //  console.log(member);
+  //    console.log(familyMember[member]);
+  // }
+
+  // const personNam = {lname: "shanto", tname: "arif", fourname:"ayath", fname: "sajeeb"};
+
+  // for(let x in personNam){
+    //  console.log(x);
+    //  console.log(personNam[x]);
+  //    console.log(personNam['fname'])
+  // }
+
+  // const numberItems = [12, 43, 23, 65, 45, 33, 86, 100];
+
+  // for(let num of numberItems){
+  //    console.log(num);
+  // }
+
+  // const num2 = [2, 4, 5, 7, 9];
+
+  // while(num2 > 8){
+  //   console.log(num2);
+  // }
+
+
+  // const car = {
+  //   name: "BMW",
+  //   model: 500,
+  //   color: "Balck",
+  //   weight: "1000kg",
+  //   start: function (){
+  //     console.log("car has started");
+  //     this.drive();
+  //   },
+  //   drive: function(){
+  //     console.log("car is driving");
+  //   }
+  // };
+  // console.log(car['name']);
+  // console.log(car['drive']);
+  // car.start();
+ 
+  
+  // const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+  // function myFunction(fol){
+  //    console.log(fol);
+  // }
+  // fruits.forEach(myFunction);
+
+  // const number = [23, 53, 12, 34, 86];
+
+  // function myfunction(value, index, array){
+  //    console.log(value);
+  //    console.log(array);
+  // }
+  // number.forEach(myfunction);
+
+
+  // const number = [23, 53, 12, 34, 86];
+
+  // function myfunction2(value){
+  //    return value * 4;
+  // }
+
+  // const newNumber = number.map(myfunction2);
+  
+  // console.log(number);
+  // console.log(newNumber);
+
+  // const num = [12, 32, 4, 2, 64, 3];
+
+  // function mynumber(value){
+  //    return value > 10;
+  // }
+  // const nums = num.filter(mynumber);
+
+  // console.log(nums);
+  // console.log(num);
+
+  //  const numberItems = [14, 29, 30, 48, 58, 65, 7];
+
+  //  function myitems(value, index, array){
+  //     return value < 20;
+    
+  //   }
+  //  const num3 = numberItems.find(myitems);
+  //  console.log(num3);
+  //  console.log(numberItems);
+
+
+  // const x = [1,2 ,3];
+
+  // console.log(x instanceof Object);
+
+  // const a = [1, 2, 3, 4, 5, 6,0];
+  // console.log(a.length);
+
+  // for(let a = 0; a < 10; a ++){
+
+  //   if(a == 6){
+  //     console.log("Break");
+  //     continue
+  //   }
+  //   console.log(a);
+  // }
+  
+  // let x = 20;
+
+  // let MyCondition = x > 10 ? "this is big num": "this is small num";
+  // console.log(MyCondition);
+  
